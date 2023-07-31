73b6822a90088bf721f238c2cf4cd4be4e2268d9
ZeynepA <zeyneppaytop@gmail.com>
2023-07-31 20:39:35 +0300

    task 27 ornek 1 and örnek 2 added

--- src/ZeynepA/task27/Ornek1/Ford.java
+++ src/ZeynepA/task27/Ornek1/Ford.java
@@ -5,7 +5,7 @@ import task27.Ornek1.BinekOto;
 public class Ford extends BinekOto {
 
     //abstract metod olduğundan zorunlu olarak
-    // yazılmak zorunda (imlemente edildi.)
+    // yazılmak zorunda (implemente edildi.)
 
     @Override
     public String getMarka() {
